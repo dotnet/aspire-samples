606337a74c7fc22426396530f7254a1cdecb11e3
James Newton-King <james@newtonking.com>
2023-11-08 12:11:29 +0800

    Add metrics sample featuring Prometheus and Grafana containers (#11)
    * Add metrics sample
    
    * Clean up
    
    * PR feedback
    
    * Add README.md

---
+++ samples/Metrics/MetricsApp/ClientApi.cs
@@ -0,0 +1,46 @@
+﻿namespace MetricsApp;
+
+public static class ClientApi
+{
+    private static readonly string[] s_summaries =
+    [
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    ];
+
+    public static void MapClientApi(this IEndpointRouteBuilder endpoints, IConfiguration configuration)
+    {
+        endpoints.MapGet("weather", async () =>
+        {
+            await Task.Delay(Random.Shared.Next(1000));
+
+            if (Random.Shared.Next(5) == 0)
+            {
+                throw new InvalidOperationException("Error getting weather data.");
+            }
+
+            var results = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = s_summaries[Random.Shared.Next(s_summaries.Length)]
+            }).ToArray();
+            return results;
+        }).RequireAuthorization();
+
+        endpoints.MapGet("startup", () =>
+        {
+            return new
+            {
+                GrafanaUrl = (string)configuration["GRAFANA_URL"]!
+            };
+        });
+    }
+
+    private sealed class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+        public int TemperatureC { get; set; }
+        public string? Summary { get; set; }
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
---
+++ samples/Metrics/MetricsApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MetricsApp;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
+builder.Services.AddAuthorizationBuilder();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("AppDb"));
+
+builder.Services.AddIdentityCore<MyUser>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddApiEndpoints();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+app.MapFallbackToFile("index.html");
+
+var api = app.MapGroup("/api");
+api.MapIdentityApi<MyUser>();
+api.MapClientApi(app.Configuration);
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseWebAssemblyDebugging();
+}
+
+app.UseStaticFiles();
+app.UseBlazorFrameworkFiles();
+
+await Auth.InitializeTestUserAsync(app.Services);
+
+app.Run();
+
