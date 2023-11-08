65801b8dea2f33c737aeb9a344e65ed8a2817aea
David Fowler <davidfowl@gmail.com>
2023-11-07 22:34:15 -0800

    Added a sample of the aspire starter with dapr - Use the dapr.aspnetcore package and change the weather client to use dapr instead of the build in service discovery.

---
+++ samples/AspireWithDapr/AspireWithDapr.Web/Program.cs
@@ -0,0 +1,37 @@
+﻿using AspireWithDapr.Web;
+using AspireWithDapr.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add service defaults & Aspire components.
+builder.AddServiceDefaults();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDaprClient();
+
+builder.Services.AddOutputCache();
+
+builder.Services.AddSingleton<WeatherApiClient>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+
+app.UseAntiforgery();
+
+app.UseOutputCache();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.MapDefaultEndpoints();
+
+app.Run();
