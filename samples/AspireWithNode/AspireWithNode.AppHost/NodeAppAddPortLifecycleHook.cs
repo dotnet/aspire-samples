aebe7d70bb1af61d222adb75167f6789df562093
Damian Edwards <damian@damianedwards.com>
2023-11-09 17:05:34 -0800

    Factor node app hosting into extensions

---
+++ samples/AspireWithNode/AspireWithNode.AppHost/NodeAppAddPortLifecycleHook.cs
@@ -0,0 +1,37 @@
+﻿using Aspire.Hosting.Lifecycle;
+
+internal class NodeAppAddPortLifecycleHook : IDistributedApplicationLifecycleHook
+{
+    public Task BeforeStartAsync(DistributedApplicationModel appModel, CancellationToken cancellationToken = default)
+    {
+        var nodeApps = appModel.Resources.OfType<NodeAppResource>();
+        foreach (var app in nodeApps)
+        {
+            if (app.TryGetServiceBindings(out var bindings))
+            {
+                var envAnnoation = new EnvironmentCallbackAnnotation(env =>
+                {
+                    var multiBindings = bindings.Count() > 1;
+
+                    if (multiBindings)
+                    {
+                        foreach (var binding in bindings)
+                        {
+                            var serviceName = multiBindings ? $"{app.Name}_{binding.Name}" : app.Name;
+                            env[$"PORT_{binding.Name.ToUpperInvariant()}"] = $"{{{{- portForServing \"{serviceName}\" -}}}}";
+                        }
+                        
+                    }
+                    else
+                    {
+                        env["PORT"] = $"{{{{- portForServing \"{app.Name}\" -}}}}";
+                    }
+                });
+
+                app.Annotations.Add(envAnnoation);
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+}
--- samples/AspireWithNode/AspireWithNode.AppHost/Program.cs
+++ samples/AspireWithNode/AspireWithNode.AppHost/Program.cs
@@ -1,19 +1,12 @@
-﻿using System.Globalization;
-using Microsoft.Extensions.Hosting;
-
-var builder = DistributedApplication.CreateBuilder(args);
+﻿var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedisContainer("cache");
 
 var weatherapi = builder.AddProject<Projects.AspireWithNode_AspNetCoreApi>("weatherapi");
 
-builder.AddExecutable("frontend", "npm", "../NodeFrontend", "run", "watch")
+builder.AddNpmApp("frontend", "../NodeFrontend", "watch")
     .WithReference(weatherapi)
     .WithReference(cache)
-    .WithOtlpExporter()
-    .WithEnvironment("NODE_ENV", builder.Environment.IsDevelopment() ? "development" : "production")
-    .WithEnvironment("PORT", "{{- portForServing \"frontend\" -}}")
-    .WithServiceBinding(scheme: "http")
-    .ExcludeFromManifest();
+    .WithServiceBinding(scheme: "http");
 
 builder.Build().Run();
