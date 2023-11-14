32246319f6acc5629bfbeb7155ed4538db362e0a
Paul Yuknewicz <paulyuk@Pauls-MBP-2.lan>
2023-11-14 09:57:05 -0800

    added state management and ttl
    Signed-off-by: Paul Yuknewicz <paulyuk@Pauls-MBP-2.lan>

--- samples/AspireWithDapr/AspireWithDapr.Web/WeatherApiClient.cs
+++ samples/AspireWithDapr/AspireWithDapr.Web/WeatherApiClient.cs
@@ -1,12 +1,29 @@
 ﻿using Dapr.Client;
 
+
 namespace AspireWithDapr.Web;
 
 public class WeatherApiClient(DaprClient daprClient)
 {
     public async Task<WeatherForecast[]> GetWeatherAsync()
     {
-        return await daprClient.InvokeMethodAsync<WeatherForecast[]>(HttpMethod.Get, "api", "weatherforecast");
+    
+       // Get the weather from the state store if it exists
+       var weather = await daprClient.GetStateAsync<WeatherForecast[]>("statestore", "weather");
+
+        if (weather is null)
+        {
+            // If it doesn't exist, get it from the weather service
+            weather = await daprClient.InvokeMethodAsync<WeatherForecast[]>(HttpMethod.Get, "api", "weatherforecast");
+    
+            await daprClient.SaveStateAsync("statestore", "weather", weather, metadata: new Dictionary<string, string>() {
+                {
+                    "ttlInSeconds", "120" 
+                }
+            });
+        }
+
+       return weather;
     }
 }
 
