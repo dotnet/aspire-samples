88a19fa500b086365d931fe0eb1b36229b0cfcb1
James Newton-King <james@newtonking.com>
2023-11-15 09:32:47 +0800

    Minor Grafana dashboard improvements (#30)
    * Minor Grafana dashboard improvements
    
    * Description
    
    * Update samples/Metrics/grafana/dashboards/aspnetcore-endpoint.json
    
    * More fixes
    
    * Add screenshot

--- samples/Metrics/MetricsApp.AppHost/Program.cs
+++ samples/Metrics/MetricsApp.AppHost/Program.cs
@@ -5,7 +5,8 @@ var grafana = builder.AddContainer("grafana", "grafana/grafana")
                      .WithVolumeMount("../grafana/dashboards", "/var/lib/grafana/dashboards")
                      .WithServiceBinding(containerPort: 3000, hostPort: 3000, name: "grafana-http", scheme: "http");
 
-builder.AddProject<Projects.MetricsApp>("app");
+builder.AddProject<Projects.MetricsApp>("app")
+       .WithEnvironment("GRAFANA_URL", grafana.GetEndpoint("grafana-http"));
 
 builder.AddContainer("prometheus", "prom/prometheus")
        .WithVolumeMount("../prometheus", "/etc/prometheus")
