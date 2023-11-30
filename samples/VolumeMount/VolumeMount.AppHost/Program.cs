e33643e251c4618403018649df83ba00b2367ae0
Damian Edwards <damian@damianedwards.com>
2023-11-30 10:25:28 -0800

    Add sample of container with persistent volume mount (#52)
    * Add sample of container with persistent volume mount
    
    Fixes #44
    
    * Don't throw for missing sqlpassword during manifest generation
    
    * Update samples/VolumeMount/VolumeMount.AppHost/Program.cs
    
    Co-authored-by: David Fowler <davidfowl@gmail.com>
    
    ---------
    
    Co-authored-by: David Fowler <davidfowl@gmail.com>

---
+++ samples/VolumeMount/VolumeMount.AppHost/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+// Using a persistent volume mount requires a stable password for 'sa' rather than the default generated one.
+var sqlpassword = builder.Configuration["sqlpassword"];
+
+if (builder.Environment.IsDevelopment() && string.IsNullOrEmpty(sqlpassword))
+{
+    throw new InvalidOperationException("""
+        A password for the local SQL Server container is not configured.
+        Add one to the AppHost project's user secrets with the key 'sqlpassword', e.g. dotnet user-secrets set sqlpassword <password>
+        """);
+}
+
+// To have a persistent volume mount across container instances, it must be named (VolumeMountType.Named).
+var database = builder.AddSqlServerContainer("sqlserver", sqlpassword)
+    .WithVolumeMount("VolumeMount.sqlserver.data", "/var/opt/mssql", VolumeMountType.Named)
+    .AddDatabase("appdb");
+
+builder.AddProject<Projects.VolumeMount_BlazorWeb>("blazorweb")
+    .WithReference(database);
+
+builder.Build().Run();
