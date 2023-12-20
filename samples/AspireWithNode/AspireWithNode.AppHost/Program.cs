f67105c266e0ca16ef65d7ae2bf1d8393b2552b8
Damian Edwards <damian@damianedwards.com>
2023-12-20 09:50:54 -0800

    Updating samples for preview.2 (#55)
    * Updating samples for preview.2
    
    * Skip sign check on workload install for now
    
    * Update NuGet.config
    
    * Update to latest preview.2 build
    
    * Update to latest preview.2 build
    
    * Fix node sample
    
    * Update the version of preview2 to the latest
    
    * Updated the dapr sample and instructions
    
    * Update Dapr README to call out dapr init (#66)
    
    * Update to latest package versions
    
    * Support deploying the node sample
    - Added a docker file
    - Work around an issue with manifest generation
    - AddRedis instead of AddRedisContainer
    
    * Update to the latest preview2
    
    * Update to latest preview.2 build
    
    * Prepare for release
    
    ---------
    
    Co-authored-by: David Fowler <davidfowl@gmail.com>
    Co-authored-by: Balamurugan Chirtsabesan <balach@microsoft.com>

--- samples/AspireWithNode/AspireWithNode.AppHost/Program.cs
+++ samples/AspireWithNode/AspireWithNode.AppHost/Program.cs
@@ -1,12 +1,13 @@
 ﻿var builder = DistributedApplication.CreateBuilder(args);
 
-var cache = builder.AddRedisContainer("cache");
+var cache = builder.AddRedis("cache");
 
 var weatherapi = builder.AddProject<Projects.AspireWithNode_AspNetCoreApi>("weatherapi");
 
 builder.AddNpmApp("frontend", "../NodeFrontend", "watch")
     .WithReference(weatherapi)
     .WithReference(cache)
-    .WithServiceBinding(scheme: "http");
+    .WithServiceBinding(containerPort: 3000, scheme: "http", env: "PORT")
+    .AsDockerfileInManifest();
 
 builder.Build().Run();
