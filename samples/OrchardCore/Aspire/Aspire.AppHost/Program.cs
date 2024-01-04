9ef501627300f572598089080ce9e10542696efa
Mike Alhayek <mike@crestapps.com>
2024-01-04 13:48:50 -0800

    Adding OrchardCore sample solution with plain ASP.NET Core project (No OC yet and everything is working)

---
+++ samples/OrchardCore/Aspire/Aspire.AppHost/Program.cs
@@ -0,0 +1,10 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var redis = builder.AddRedisContainer("Redis", 50963);
+
+builder.AddProject<Projects.OrchardCore_Cms>("OrchardCore CMS")
+    .WithReference(redis);
+
+builder.AddProject<Projects.OrchardCore_Mvc>("OrchardCore MVC");
+
+builder.Build().Run();
---
+++ samples/OrchardCore/OrchardCore.Mvc/Program.cs
@@ -0,0 +1,10 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+app.MapGet("/", () => "MVC App");
+
+app.Run();
