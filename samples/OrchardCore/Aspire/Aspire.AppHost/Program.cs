ff02926e61c59c2f99c57536a984073470be6136
Mike Alhayek <mike@crestapps.com>
2024-01-05 09:59:20 -0800

    use enviornment variables

--- samples/OrchardCore/Aspire/Aspire.AppHost/Program.cs
+++ samples/OrchardCore/Aspire/Aspire.AppHost/Program.cs
@@ -1,12 +1,17 @@
 ﻿var builder = DistributedApplication.CreateBuilder(args);
+const int postgresPort = 62262;
+const string postgresUsername = "occms";
+const string postgresPassword = "OrchardCorePass";
 
-var cmsdb = builder.AddPostgresContainer("Postgres", 62262, "OrchardCorePass")
-    .WithEnvironment("POSTGRES_USER", "occms")
-    .WithEnvironment("POSTGRES_PASSWORD", "OrchardCorePass");
+var cmsdb = builder.AddPostgresContainer("Postgres", postgresPort, postgresPassword);
     
 var redis = builder.AddRedisContainer("Redis", 50963);
 
 builder.AddProject<Projects.OrchardCore_Cms>("OrchardCore CMS")
+    .WithEnvironment("OrchardCore__Default__State", "Uninitialized")
+    .WithEnvironment("OrchardCore__Default__TablePrefix", "Default")
+    .WithEnvironment("OrchardCore__DatabaseProvider", "Postgres")
+    .WithEnvironment("OrchardCore__ConnectionString", $"host=localhost;port={postgresPort};database={postgresUsername};username={postgresUsername};password={postgresPassword}")
     .WithReference(redis)
     .WithReference(cmsdb);
 
