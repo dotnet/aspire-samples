6f4e6943228b10d3a7d81d83f532054ef1451a81
Nick Randolph <nick@builttoroam.com>
2024-01-07 02:18:27 +1100

    feat: Adding winui sample

---
+++ samples/ClientAppsIntegration/ClientAppsIntegration.WinUI/WeatherApiClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClientAppsIntegration.WinUI;
+
+public class WeatherApiClient(HttpClient httpClient)
+{
+    public async Task<WeatherForecast[]> GetWeatherAsync(CancellationToken cancellationToken = default)
+    {
+        return await httpClient.GetFromJsonAsync<WeatherForecast[]>("/weatherforecast", cancellationToken) ?? [];
+    }
+}
+
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
