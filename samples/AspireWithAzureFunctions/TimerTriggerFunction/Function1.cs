24034a547bb06f5dbd384f335ebc349e6f661959
Joe Whiteaker <47674962+josephaw1022@users.noreply.github.com>
2024-01-06 19:08:21 -0500

    create a sample with two simple projects

---
+++ samples/AspireWithAzureFunctions/TimerTriggerFunction/Function1.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace TimerTriggerFunction
+{
+    public class Function1
+    {
+        private readonly ILogger _logger;
+
+        public Function1(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<Function1>();
+        }
+
+        [Function("Function1")]
+        public void Run([TimerTrigger("*/30 * * * * *")] TimerInfo myTimer)
+        {
+            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            
+            if (myTimer.ScheduleStatus is not null)
+            {
+                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+            }
+        }
+    }
+}
