a616aa83602afc2d9ad9dd61fec69e1c09f3ba47
Joe Whiteaker <47674962+josephaw1022@users.noreply.github.com>
2024-01-12 19:57:09 -0500

    progress in yarp example

---
+++ samples/AspireWithYarp/FeedMicroservice/Program.cs
@@ -0,0 +1,31 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.MapGet("/", () =>
+{
+    var feedMessage  = "This is the feed message microservice";
+    
+    return Results.Ok(feedMessage);
+})
+.WithName("GetFeedPosts")
+.WithOpenApi();
+
+app.Run();
---
+++ samples/AspireWithYarp/PublicSiteBetaVersion/Program.cs
@@ -0,0 +1,38 @@
+﻿using BlazorPublicSiteBetaVersion.Components;
+
+namespace PublicSiteBetaVersion;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.AddServiceDefaults();
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        var app = builder.Build();
+
+        app.MapDefaultEndpoints();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
+}
