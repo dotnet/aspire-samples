548e3d9a609d5992ac68f9cabaef01ec659f40af
David Pine <david.pine@microsoft.com>
2024-02-23 10:49:22 -0600

    Use exposed container port 80

--- samples/AspireWithJavaScript/AspireJavaScript.AppHost/Program.cs
+++ samples/AspireWithJavaScript/AspireJavaScript.AppHost/Program.cs
@@ -6,19 +6,19 @@ var weatherApi =
 // Angular: npm run start
 builder.AddNpmApp("angular", "../AspireJavaScript.Angular")
     .WithReference(weatherApi)
-    .WithEndpoint(containerPort: 3000, scheme: "http", env: "PORT")
+    .WithHttpEndpoint(containerPort: 80, env: "PORT")
     .AsDockerfileInManifest();
 
 // React: npm run start
 builder.AddNpmApp("react", "../AspireJavaScript.React")
     .WithReference(weatherApi)
-    .WithEndpoint(containerPort: 3001, scheme: "http", env: "PORT")
+    .WithHttpEndpoint(containerPort: 80, env: "PORT")
     .AsDockerfileInManifest();
 
 // Vue: npm run dev
 builder.AddNpmApp("vue", "../AspireJavaScript.Vue", "dev")
     .WithReference(weatherApi)
-    .WithEndpoint(containerPort: 3002, scheme: "http", env: "PORT")
+    .WithHttpEndpoint(containerPort: 80, env: "PORT")
     .AsDockerfileInManifest();
 
 builder.Build().Run();
