5459a6c75bd99d1c461cadccb8ae54b947a80a13
James Newton-King <james@newtonking.com>
2024-03-01 07:17:54 +0800

    Add standalone dashboard sample (#135)

---
+++ samples/StandaloneDashboard/ConsoleApp/Model/PokemonList.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp.Model;
+
+internal class PokemonList
+{
+    public int Count { get; init; }
+    public required string? Next { get; init; }
+    public required string? Previous { get; init; }
+    public required List<Pokemon> Results { get; init; }
+}
