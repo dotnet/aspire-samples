c46017e704997f4e2baace0f4472028a3aa75d0c
Damian Edwards <damian@damianedwards.com>
2024-02-28 17:08:03 -0800

    Rename eShopLite sample to AspireShop

--- samples/eShopLite/eShopLite.BasketService/Models/Order.cs
+++
@@ -1,9 +0,0 @@
-namespace eShopLite.BasketService.Models;
-
-public class Order
-{
-    public required string Id { get; set; }
-    public string? BuyerId { get; set; }
-
-    public List<BasketItem> Items { get; set; } = new();
-}
