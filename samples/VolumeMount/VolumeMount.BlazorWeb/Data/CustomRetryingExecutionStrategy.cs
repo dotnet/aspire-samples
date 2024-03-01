277fb788cf6d528bbf496f521f0831a42416d593
Damian Edwards <damian@damianedwards.com>
2024-03-01 11:09:42 -0800

    PR feedback

--- samples/VolumeMount/VolumeMount.BlazorWeb/Data/CustomRetryingExecutionStrategy.cs
+++ samples/VolumeMount/VolumeMount.BlazorWeb/Data/CustomRetryingExecutionStrategy.cs
@@ -12,6 +12,7 @@ public class CustomRetryingExecutionStrategy(ExecutionStrategyDependencies depen
         {
             foreach (SqlError error in sqlException.Errors)
             {
+                // EF Core issue logged to consider making this a default https://github.com/dotnet/efcore/issues/33191
                 if (error.Number == 4060)
                 {
                     // Don't retry on login failures associated with default database not existing due to EF migrations not running yet
