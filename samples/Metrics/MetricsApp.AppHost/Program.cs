6ad836aba744611f86b0a9c1447943361ccf3aa9
Damian Edwards <damian@damianedwards.com>
2024-04-01 13:42:08 -0700

    Run npm install on build in JavaScript sample

--- samples/Metrics/MetricsApp.AppHost/Program.cs
+++ samples/Metrics/MetricsApp.AppHost/Program.cs
@@ -3,14 +3,14 @@
 var grafana = builder.AddContainer("grafana", "grafana/grafana")
                      .WithBindMount("../grafana/config", "/etc/grafana", isReadOnly: true)
                      .WithBindMount("../grafana/dashboards", "/var/lib/grafana/dashboards", isReadOnly: true)
-                     .WithEndpoint(targetPort: 3000, port: 3000, name: "grafana-http", scheme: "http");
+                     .WithEndpoint(targetPort: 3000, name: "grafana-http", scheme: "http");
 
 builder.AddProject<Projects.MetricsApp>("app")
        .WithEnvironment("GRAFANA_URL", grafana.GetEndpoint("grafana-http"));
 
 builder.AddContainer("prometheus", "prom/prometheus")
        .WithBindMount("../prometheus", "/etc/prometheus", isReadOnly: true)
-       .WithEndpoint(targetPort: 9090, port: 9090);
+       .WithEndpoint(/* This port is fixed as it's referenced from the Grafana config */ port: 9090, targetPort: 9090);
 
 using var app = builder.Build();
 
