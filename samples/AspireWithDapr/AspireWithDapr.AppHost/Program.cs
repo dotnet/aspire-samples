7c50b04c180e91ba7c343e6285ae61ee2305cb98
Damian Edwards <damian@damianedwards.com>
2024-04-19 12:16:03 -0700

    Work around Dapr CLI path issue

--- samples/AspireWithDapr/AspireWithDapr.AppHost/Program.cs
+++ samples/AspireWithDapr/AspireWithDapr.AppHost/Program.cs
@@ -1,9 +1,22 @@
-﻿var builder = DistributedApplication.CreateBuilder(args);
+﻿using Aspire.Hosting.Dapr;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.AspireWithDapr_ApiService>("apiservice")
        .WithDaprSidecar("api");
 
 builder.AddProject<Projects.AspireWithDapr_Web>("webfrontend")
        .WithDaprSidecar("web");
+
+// Workaround for https://github.com/dotnet/aspire/issues/2219
+if (builder.Configuration.GetValue<string>("DAPR_CLI_PATH") is { } daprCliPath)
+{
+    builder.Services.Configure<DaprOptions>(options =>
+    {
+        options.DaprPath = daprCliPath;
+    });
+}
 
 builder.Build().Run();
