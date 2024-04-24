99106cc0d58feb2329476237a6a0b432ee093b61
Mitch Denny <midenn@microsoft.com>
2024-04-24 13:44:34 +1000

    MailDev custom resource POC.

---
+++ samples/CustomResource/CustomResource.AppHost/Program.cs
@@ -0,0 +1,6 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+var maildev = builder.AddMailDev("maildev");
+builder.AddProject<Projects.CustomResource_SampleApp>("customresource-sampleapp")
+       .WithReference(maildev);
+
+builder.Build().Run();
