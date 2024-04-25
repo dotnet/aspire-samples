762582fbb2122035f02acdfa0a0485dd3f3c03cb
David Pine <david.pine@microsoft.com>
2024-04-25 11:19:49 -0500

    Remove DockerBuildArgs as it doesn't work with azd

--- samples/AspireWithJavaScript/AspireJavaScript.AppHost/Program.cs
+++ samples/AspireWithJavaScript/AspireJavaScript.AppHost/Program.cs
@@ -3,29 +3,23 @@
 var weatherApi = builder.AddProject<Projects.AspireJavaScript_MinimalApi>("weatherapi")
     .WithExternalHttpEndpoints();
 
-var buildArgs = new DockerBuildArg[]
-{
-    new("services__weatherapi__http__0"),
-    new("services__weatherapi__https__0"),
-};
-
 builder.AddNpmApp("angular", "../AspireJavaScript.Angular")
     .WithReference(weatherApi)
     .WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints()
-    .PublishAsDockerFile(buildArgs);
+    .PublishAsDockerFile();
 
 builder.AddNpmApp("react", "../AspireJavaScript.React")
     .WithReference(weatherApi)
     .WithEnvironment("BROWSER", "none") // Disable opening browser on npm start
     .WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints()
-    .PublishAsDockerFile(buildArgs);
+    .PublishAsDockerFile();
 
 builder.AddNpmApp("vue", "../AspireJavaScript.Vue")
     .WithReference(weatherApi)
     .WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints()
-    .PublishAsDockerFile(buildArgs);
+    .PublishAsDockerFile();
 
 builder.Build().Run();
