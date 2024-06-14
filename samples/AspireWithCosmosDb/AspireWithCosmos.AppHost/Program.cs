789446c77be8f25190c51afa8779099d616ea680
bradygaster <bradyg@microsoft.com>
2024-06-14 15:32:43 -0700

    added emulator call and comment.

--- samples/AspireWithCosmosDb/AspireWithCosmos.AppHost/Program.cs
+++ samples/AspireWithCosmosDb/AspireWithCosmos.AppHost/Program.cs
@@ -1,9 +1,11 @@
 ﻿var builder = DistributedApplication.CreateBuilder(args);
 
-var cosmos = builder.AddAzureCosmosDB("cosmos");
+var tododb = builder.AddAzureCosmosDB("cosmos").AddDatabase("tododb")
+    .RunAsEmulator(); // remove this line should you want to use a live instance during development
+                      // or should you not have the Azure Cosmos DB emulator installed.
 
 var apiService = builder.AddProject<Projects.AspireWithCosmos_ApiService>("apiservice")
-    .WithReference(cosmos);
+    .WithReference(tododb);
 
 builder.AddProject<Projects.AspireWithCosmos_Web>("webfrontend")
     .WithExternalHttpEndpoints()
