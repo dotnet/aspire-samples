c04b9e373ba33fb39438ad33e65da7ca417ee973
bradygaster <bradyg@microsoft.com>
2024-06-14 12:47:36 -0700

    formatting

--- samples/AspireWithCosmosDb/AspireWithCosmos.AppHost/Program.cs
+++ samples/AspireWithCosmosDb/AspireWithCosmos.AppHost/Program.cs
@@ -2,8 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cosmos = builder.AddAzureCosmosDB("cosmos");
 
-var apiService = builder.AddProject<Projects.AspireWithCosmos_ApiService>("apiservice")
-                        .WithReference(cosmos);
+var apiService = builder.AddProject<Projects.AspireWithCosmos_ApiService>("apiservice").WithReference(cosmos);
 
 builder.AddProject<Projects.AspireWithCosmos_Web>("webfrontend")
     .WithExternalHttpEndpoints()
