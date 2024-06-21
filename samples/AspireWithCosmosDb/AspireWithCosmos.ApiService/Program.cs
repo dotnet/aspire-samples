c0352938c676eef03b9a1e678161c87ee094e413
Damian Edwards <damian@damianedwards.com>
2024-06-21 16:03:08 -0700

    Add retry policy & health check for Cosmos DB db creation

--- samples/AspireWithCosmosDb/AspireWithCosmos.ApiService/Program.cs
+++ samples/AspireWithCosmosDb/AspireWithCosmos.ApiService/Program.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Polly;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 builder.Services.AddProblemDetails();
-builder.Services.AddHostedService<DatabaseBootstrapper>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApiDocument();
+
 builder.AddAzureCosmosClient("cosmos");
+builder.Services.AddSingleton<DatabaseBootstrapper>();
+builder.Services.AddHealthChecks()
+    .Add(new("cosmos", sp => sp.GetRequiredService<DatabaseBootstrapper>(), null, null));
+builder.Services.AddHostedService(sp => sp.GetRequiredService<DatabaseBootstrapper>());
 
 var app = builder.Build();
 
@@ -19,14 +26,17 @@ if (app.Environment.IsDevelopment())
 
 app.UseExceptionHandler();
 
-// create new todos
+// Create new todos
 app.MapPost("/todos", async (Todo todo, CosmosClient cosmosClient) =>
     (await cosmosClient.GetAppDataContainer().CreateItemAsync(todo)).Resource
 );
 
-// get all the todos
+// Get all the todos
 app.MapGet("/todos", (CosmosClient cosmosClient) =>
-    cosmosClient.GetAppDataContainer().GetItemLinqQueryable<Todo>(allowSynchronousQueryExecution: true).ToList()
+    cosmosClient.GetAppDataContainer()
+                .GetItemLinqQueryable<Todo>()
+                .ToFeedIterator()
+                .ToAsyncEnumerable()
 );
 
 app.MapPut("/todos/{id}", async (string id, Todo todo, CosmosClient cosmosClient) =>
@@ -51,16 +61,59 @@ public record Todo(string Description, string id, string UserId, bool IsComplete
 }
 
 // Background service used to scaffold the Cosmos DB/Container
-public class DatabaseBootstrapper(CosmosClient cosmosClient) : IHostedService
+public class DatabaseBootstrapper(CosmosClient cosmosClient, ILogger<DatabaseBootstrapper> logger) : BackgroundService, IHealthCheck
 {
-    public async Task StartAsync(CancellationToken cancellationToken)
+    private bool _dbCreated;
+    private bool _dbCreationFailed;
+
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
     {
-        await cosmosClient.CreateDatabaseIfNotExistsAsync("tododb", cancellationToken: cancellationToken);
-        var database = cosmosClient.GetDatabase("tododb");
-        await database.CreateContainerIfNotExistsAsync(new ContainerProperties("todos", Todo.UserIdPartitionKey), cancellationToken: cancellationToken);
+        var status = _dbCreated
+            ? HealthCheckResult.Healthy()
+            : _dbCreationFailed
+                ? HealthCheckResult.Unhealthy("Database creation failed.")
+                : HealthCheckResult.Degraded("Database creation is still in progress.");
+        return Task.FromResult(status);
     }
 
-    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        // The Cosmos DB emulator can take a very long time to start (multiple minutes) so use a custom resilience strategy
+        // to ensure it retries long enough.
+        var retry = new ResiliencePipelineBuilder()
+            .AddRetry(new()
+            {
+                Delay = TimeSpan.FromSeconds(5),
+                MaxRetryAttempts = 60,
+                BackoffType = DelayBackoffType.Constant,
+                OnRetry = args =>
+                {
+                    logger.LogWarning("""
+                        Issue during database creation after {AttemptDuration} on attempt {AttemptNumber}. Will retry in {RetryDelay}.
+                        Exception:
+                            {ExceptionMessage}
+                            {InnerExceptionMessage}
+                        """,
+                        args.Duration,
+                        args.AttemptNumber,
+                        args.RetryDelay,
+                        args.Outcome.Exception?.Message ?? "[none]",
+                        args.Outcome.Exception?.InnerException?.Message ?? "");
+                    return ValueTask.CompletedTask;
+                }
+            })
+            .Build();
+        await retry.ExecuteAsync(async ct =>
+        {
+            await cosmosClient.CreateDatabaseIfNotExistsAsync("tododb", cancellationToken: ct);
+            var database = cosmosClient.GetDatabase("tododb");
+            await database.CreateContainerIfNotExistsAsync(new ContainerProperties("todos", Todo.UserIdPartitionKey), cancellationToken: ct);
+            logger.LogInformation("Database successfully created!");
+            _dbCreated = true;
+        }, stoppingToken);
+
+        _dbCreationFailed = !_dbCreated;
+    }
 }
 
 // Convenience class for reusing boilerplate code
@@ -72,5 +125,16 @@ public static class CosmosClientTodoAppExtensions
         var todos = database.GetContainer("todos") ?? throw new ApplicationException("Cosmos DB collection missing.");
 
         return todos;
+    }
+
+    public static async IAsyncEnumerable<TModel> ToAsyncEnumerable<TModel>(this FeedIterator<TModel> setIterator)
+    {
+        while (setIterator.HasMoreResults)
+        {
+            foreach (var item in await setIterator.ReadNextAsync())
+            {
+                yield return item;
+            }
+        }
     }
 }
--- samples/AspireWithCosmosDb/AspireWithCosmos.AppHost/Program.cs
+++ samples/AspireWithCosmosDb/AspireWithCosmos.AppHost/Program.cs
@@ -1,8 +1,10 @@
-﻿var builder = DistributedApplication.CreateBuilder(args);
+﻿using Aspire.Hosting;
+
+var builder = DistributedApplication.CreateBuilder(args);
 
 var tododb = builder.AddAzureCosmosDB("cosmos").AddDatabase("tododb")
-    .RunAsEmulator(); // Remove this line should you want to use a live instance during development
-                      // or should you not have the Azure Cosmos DB emulator installed.
+    // Remove the RunAsEmulator() line should you want to use a live instance during development
+    .RunAsEmulator(); 
 
 var apiService = builder.AddProject<Projects.AspireWithCosmos_ApiService>("apiservice")
     .WithReference(tododb);
