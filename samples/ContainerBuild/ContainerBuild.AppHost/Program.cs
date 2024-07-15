05ca72ebceacf636d547986fc619ba0c79c386ec
Mitch Denny <midenn@microsoft.com>
2024-07-15 19:06:58 +1000

    Add container build sample.

---
+++ samples/ContainerBuild/ContainerBuild.AppHost/Program.cs
@@ -0,0 +1,10 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var goVersion = builder.AddParameter("goversion"); // Value set in appsettings.json and overrides the default
+                                                   // specified in the Dockerfile.
+
+builder.AddDockerfile("ginapp", "..\\ginapp")
+       .WithEndpoint(scheme: "http", targetPort: 5555, env: "PORT")
+       .WithBuildArg("GO_VERSION", goVersion);
+
+builder.Build().Run();
