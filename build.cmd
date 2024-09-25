ce43132034ead5b7009893b1d5acb8521325478d
Jose Perez Rodriguez <joperezr@microsoft.com>
2024-09-25 13:35:06 -0700

    Update to Aspire 9.0

--- build.cmd
+++ build.cmd
@@ -1,6 +1,3 @@
 @ECHO OFF
 
-dotnet workload update
-dotnet workload install aspire
-
 dotnet build .\build\Build.proj
--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-dotnet workload update
-dotnet workload install aspire
-
 dotnet build ./build/Build.proj
