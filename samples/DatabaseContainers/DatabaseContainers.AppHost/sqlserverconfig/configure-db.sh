887ab30aa2b023f5c0134181541fcd7e14b2793d
Damian Edwards <damian@damianedwards.com>
2024-10-16 15:13:56 -0700

    Update DatabaseContainers sample to use new features

--- samples/DatabaseContainers/DatabaseContainers.AppHost/sqlserverconfig/configure-db.sh
+++ samples/DatabaseContainers/DatabaseContainers.AppHost/sqlserverconfig/configure-db.sh
@@ -4,7 +4,7 @@
 
 # Adapted from: https://github.com/microsoft/mssql-docker/blob/80e2a51d0eb1693f2de014fb26d4a414f5a5add5/linux/preview/examples/mssql-customize/configure-db.sh
 
-# Wait 60 seconds for SQL Server to start up by ensuring that
+# Wait 120 seconds for SQL Server to start up by ensuring that
 # calling SQLCMD does not return an error code, which will ensure that sqlcmd is accessible
 # and that system and user databases return "0" which means all databases are in an "online" state
 # https://docs.microsoft.com/en-us/sql/relational-databases/system-catalog-views/sys-databases-transact-sql?view=sql-server-2017
@@ -12,7 +12,7 @@
 dbstatus=1
 errcode=1
 start_time=$SECONDS
-end_by=$((start_time + 60))
+end_by=$((start_time + 120))
 
 echo "Starting check for SQL Server start-up at $start_time, will end at $end_by"
 
@@ -26,7 +26,7 @@ elapsed_time=$((SECONDS - start_time))
 echo "Stopped checking for SQL Server start-up after $elapsed_time seconds (dbstatus=$dbstatus,errcode=$errcode,seconds=$SECONDS)"
 
 if [[ $dbstatus -ne 0 ]] || [[ $errcode -ne 0 ]]; then
-    echo "SQL Server took more than 60 seconds to start up or one or more databases are not in an ONLINE state"
+    echo "SQL Server took more than 120 seconds to start up or one or more databases are not in an ONLINE state"
     echo "dbstatus = $dbstatus"
     echo "errcode = $errcode"
     exit 1
