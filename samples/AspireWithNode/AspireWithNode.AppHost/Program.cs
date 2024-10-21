b32ab7742f1b8018249acca86038e1fd0ee35182
Damian Edwards <damian@damianedwards.com>
2024-10-21 16:19:53 -0700

    Updated ApsireWithNode to use WaitFor

--- samples/AspireWithNode/AspireWithNode.AppHost/Program.cs
+++ samples/AspireWithNode/AspireWithNode.AppHost/Program.cs
@@ -8,13 +8,16 @@ var weatherapi = builder.AddProject<Projects.AspireWithNode_AspNetCoreApi>("weat
 
 var frontend = builder.AddNpmApp("frontend", "../NodeFrontend", "watch")
     .WithReference(weatherapi)
+    .WaitFor(weatherapi)
     .WithReference(cache)
+    .WaitFor(cache)
     .WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints()
     .PublishAsDockerFile();
 
 var launchProfile = builder.Configuration["DOTNET_LAUNCH_PROFILE"] ??
     builder.Configuration["AppHost:DefaultLaunchProfileName"]; // work around https://github.com/dotnet/aspire/issues/5093
+
 if (builder.Environment.IsDevelopment() && launchProfile == "https")
 {
     // Disable TLS certificate validation in development, see https://github.com/dotnet/aspire/issues/3324 for more details.
