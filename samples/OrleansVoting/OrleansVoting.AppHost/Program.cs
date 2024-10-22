f75204d15b3c5e4ac4520212e2e7e9757ff2f0d3
Damian Edwards <damian@damianedwards.com>
2024-10-21 18:12:11 -0700

    Update Orleans sample

--- samples/OrleansVoting/OrleansVoting.AppHost/Program.cs
+++ samples/OrleansVoting/OrleansVoting.AppHost/Program.cs
@@ -1,12 +1,14 @@
 ﻿var builder = DistributedApplication.CreateBuilder(args);
 
 var redis = builder.AddRedis("voting-redis");
+
 var orleans = builder.AddOrleans("voting-cluster")
     .WithClustering(redis)
     .WithGrainStorage("votes", redis);
 
 builder.AddProject<Projects.OrleansVoting_Service>("voting-fe")
     .WithReference(orleans)
+    .WaitFor(redis)
     .WithReplicas(3);
 
 builder.Build().Run();
