b368b2e6f6b56cf386d5f8b6ca7374a26a3a9d9e
Damian Edwards <damian@damianedwards.com>
2024-10-22 22:28:15 -0700

    Update python sample to enable OTLP tracing & auto-pip install

--- samples/AspireWithPython/InstrumentedPythonProject/main.py
+++ samples/AspireWithPython/InstrumentedPythonProject/main.py
@@ -1,17 +1,30 @@
 import os
-import flask
 import logging
-
-logging.basicConfig()
-logging.getLogger().setLevel(logging.NOTSET)
+import flask
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
 
 app = flask.Flask(__name__)
 
+trace.set_tracer_provider(TracerProvider())
+otlpExporter = OTLPSpanExporter()
+processor = BatchSpanProcessor(otlpExporter)
+trace.get_tracer_provider().add_span_processor(processor)
+
+FlaskInstrumentor().instrument_app(app)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 @app.route('/', methods=['GET'])
 def hello_world():
-    logging.getLogger(__name__).info("request received!")
+    logger.info("request received!")
     return 'Hello, World!'
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8111))
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=port, debug=True)
+    
