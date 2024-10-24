bd7f0bb9f0e17bc7b74cf2d272d4081fc793c6e6
Damian Edwards <damian@damianedwards.com>
2024-10-23 17:53:31 -0700

    Update to Aspire 9.1

--- samples/AspireWithPython/AspireWithPython.AppHost/Program.cs
+++ samples/AspireWithPython/AspireWithPython.AppHost/Program.cs
@@ -1,7 +1,9 @@
 ﻿var builder = DistributedApplication.CreateBuilder(args);
 
+#pragma warning disable ASPIREHOSTINGPYTHON001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 builder.AddPythonApp("instrumented-python-app", "../InstrumentedPythonProject", "main.py")
        .WithEndpoint(scheme: "http", env: "PORT")
        .WithOtlpExporter();
+#pragma warning restore ASPIREHOSTINGPYTHON001
 
 builder.Build().Run();
