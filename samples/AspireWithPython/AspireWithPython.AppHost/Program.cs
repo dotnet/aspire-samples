eeebaf1cea13dc00472174c6219572c7f5815b0e
Damian Edwards <damian@damianedwards.com>
2024-10-28 10:05:11 -0700

    WIP

--- samples/AspireWithPython/AspireWithPython.AppHost/Program.cs
+++ samples/AspireWithPython/AspireWithPython.AppHost/Program.cs
@@ -1,7 +1,7 @@
 ﻿var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddPythonApp("instrumented-python-app", "../InstrumentedPythonProject", "main.py")
-       .WithEndpoint(scheme: "http", env: "PORT")
+       .WithHttpEndpoint(env: "PORT")
        .WithOtlpExporter();
 
 builder.Build().Run();
--- samples/AspireWithPython/InstrumentedPythonProject/main.py
+++ samples/AspireWithPython/InstrumentedPythonProject/main.py
@@ -1,20 +1,20 @@
 import os
 import logging
 import flask
-from opentelemetry import trace
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
+# from opentelemetry import trace
+# from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+# from opentelemetry.sdk.trace import TracerProvider
+# from opentelemetry.sdk.trace.export import BatchSpanProcessor
+# from opentelemetry.instrumentation.flask import FlaskInstrumentor
 
 app = flask.Flask(__name__)
 
-trace.set_tracer_provider(TracerProvider())
-otlpExporter = OTLPSpanExporter()
-processor = BatchSpanProcessor(otlpExporter)
-trace.get_tracer_provider().add_span_processor(processor)
+# trace.set_tracer_provider(TracerProvider())
+# otlpExporter = OTLPSpanExporter()
+# processor = BatchSpanProcessor(otlpExporter)
+# trace.get_tracer_provider().add_span_processor(processor)
 
-FlaskInstrumentor().instrument_app(app)
+# FlaskInstrumentor().instrument_app(app)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -27,4 +27,3 @@ def hello_world():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8111))
     app.run(port=port, debug=True)
-    
