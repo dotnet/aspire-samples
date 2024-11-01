09f2a3689d22cd4ba26ef34839e876cdbaf42b92
bradygaster <bradyg@microsoft.com>
2024-11-01 13:15:24 -0700

    formatting feedback

--- samples/AspireWithAzureFunctions/ImageGallery.AppHost/Program.cs
+++ samples/AspireWithAzureFunctions/ImageGallery.AppHost/Program.cs
@@ -4,11 +4,10 @@ var storage = builder.AddAzureStorage("storage").RunAsEmulator();
 var blobs = storage.AddBlobs("blobs");
 var queues = storage.AddQueues("queues");
 
-var functions =
-    builder.AddAzureFunctionsProject<Projects.ImageGallery_Functions>("functions")
-           .WithReference(queues)
-           .WithReference(blobs)
-           .WaitFor(storage);
+var functions = builder.AddAzureFunctionsProject<Projects.ImageGallery_Functions>("functions")
+                       .WithReference(queues)
+                       .WithReference(blobs)
+                       .WaitFor(storage);
 
 builder.AddProject<Projects.ImageGallery_FrontEnd>("frontend")
        .WithReference(queues)
