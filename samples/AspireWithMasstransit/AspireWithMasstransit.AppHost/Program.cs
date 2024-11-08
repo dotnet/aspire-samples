2fbc6dca2834811b88422255b3ab1d33deb3c3d2
Bao To Quoc <baotoq@outlook.com>
2024-11-08 20:03:40 +0700

    remove old version

--- samples/AspireWithMasstransit/AspireWithMasstransit.AppHost/Program.cs
+++
@@ -1,8 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-var messaging = builder.AddRabbitMQ("messaging");
-
-var apiService = builder.AddProject<Projects.AspireWithMasstransit_ApiService>("apiservice")
-    .WithReference(messaging);
-
-builder.Build().Run();
