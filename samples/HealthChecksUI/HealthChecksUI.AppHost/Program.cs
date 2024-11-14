e052bcbd7ce1e5261149618ddecb09f0bf90f748
Damian Edwards <damian@damianedwards.com>
2024-11-14 14:33:16 -0800

    PR feedback

--- samples/HealthChecksUI/HealthChecksUI.AppHost/Program.cs
+++ samples/HealthChecksUI/HealthChecksUI.AppHost/Program.cs
@@ -13,9 +13,7 @@ var webFrontend = builder.AddProject<Projects.HealthChecksUI_Web>("webfrontend")
 
 builder.AddHealthChecksUI("healthchecksui")
     .WithReference(apiService)
-    .WaitFor(apiService)
     .WithReference(webFrontend)
-    .WaitFor(webFrontend)
     // This will make the HealthChecksUI dashboard available from external networks when deployed.
     // In a production environment, you should consider adding authentication to the ingress layer
     // to restrict access to the dashboard.
