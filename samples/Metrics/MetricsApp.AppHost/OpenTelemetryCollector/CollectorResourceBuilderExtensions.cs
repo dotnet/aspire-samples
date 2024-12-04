e1d0e5cc03458495479956ce8502b33b7c6c3144
James Newton-King <james@newtonking.com>
2024-12-04 18:16:28 +0800

    PR feedback

--- samples/Metrics/MetricsApp.AppHost/OpenTelemetryCollector/CollectorResourceBuilderExtensions.cs
+++ samples/Metrics/MetricsApp.AppHost/OpenTelemetryCollector/CollectorResourceBuilderExtensions.cs
@@ -14,7 +14,7 @@ public static class CollectorResourceBuilderExtensions
 
         var url = builder.Configuration[DashboardOtlpUrlVariableName] ?? DashboardOtlpUrlDefaultValue;
 
-        var dashboardOtlpEndpoint = ReplaceLocalhostWithContainerHost(url, builder.Configuration);
+        var dashboardOtlpEndpoint = new HostUrl(url);
         var dashboardInsecure = url.StartsWith("https", StringComparison.OrdinalIgnoreCase) ? "false" : "true";
 
         var resource = new CollectorResource(name);
@@ -23,17 +23,8 @@ public static class CollectorResourceBuilderExtensions
             .WithEndpoint(port: 4317, targetPort: 4317, name: CollectorResource.OtlpGrpcEndpointName, scheme: "http")
             .WithEndpoint(port: 4318, targetPort: 4318, name: CollectorResource.OtlpHttpEndpointName, scheme: "http")
             .WithBindMount(configFileLocation, "/etc/otelcol-contrib/config.yaml")
-            .WithEnvironment("ASPIRE_ENDPOINT", dashboardOtlpEndpoint)
+            .WithEnvironment("ASPIRE_ENDPOINT", $"{dashboardOtlpEndpoint}")
             .WithEnvironment("ASPIRE_API_KEY", builder.Configuration[DashboardOtlpApiKeyVariableName])
             .WithEnvironment("ASPIRE_INSECURE", dashboardInsecure);
-    }
-
-    private static string ReplaceLocalhostWithContainerHost(string value, IConfiguration configuration)
-    {
-        var hostName = configuration["AppHost:ContainerHostname"] ?? "host.docker.internal";
-
-        return value.Replace("localhost", hostName, StringComparison.OrdinalIgnoreCase)
-                    .Replace("127.0.0.1", hostName)
-                    .Replace("[::1]", hostName);
     }
 }
