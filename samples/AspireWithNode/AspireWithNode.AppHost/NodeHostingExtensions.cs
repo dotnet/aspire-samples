14d4540a0ef83a7521f0e861b090459865f4a03a
Damian Edwards <damian@damianedwards.com>
2025-02-03 10:25:13 -0800

    Add improvements to dev cert extensions

--- samples/AspireWithNode/AspireWithNode.AppHost/NodeHostingExtensions.cs
+++ samples/AspireWithNode/AspireWithNode.AppHost/NodeHostingExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Aspire.Hosting;
 
@@ -8,24 +9,25 @@ internal static class NodeHostingExtensions
     /// Injects the ASP.NET Core HTTPS developer certificate into the resource via the specified environment variables when
     /// <paramref name="builder"/>.<see cref="IResourceBuilder{T}.ApplicationBuilder">ApplicationBuilder</see>.<see cref="IDistributedApplicationBuilder.ExecutionContext">ExecutionContext</see>.<see cref="DistributedApplicationExecutionContext.IsRunMode">IsRunMode</see><c> == true</c>.<br/>
     /// </summary>
-    public static IResourceBuilder<NodeAppResource> RunWithHttpsDevCertificate(this IResourceBuilder<NodeAppResource> builder, string certFileEnv, string certKeyFileEnv)
+    public static IResourceBuilder<NodeAppResource> RunWithHttpsDevCertificate(this IResourceBuilder<NodeAppResource> builder, CertificateFileFormat certificateFileFormat, string certFileEnv, string? certPasswordOrKeyEnv)
     {
         if (builder.ApplicationBuilder.ExecutionContext.IsRunMode && builder.ApplicationBuilder.Environment.IsDevelopment())
         {
-            DevCertHostingExtensions.RunWithHttpsDevCertificate(builder, certFileEnv, certKeyFileEnv, (certFilePath, certKeyPath) =>
+            DevCertHostingExtensions.RunWithHttpsDevCertificate(builder, certificateFileFormat, certFileEnv, certPasswordOrKeyEnv, (services, certFilePath, certPasswordOrKeyPath) =>
             {
                 builder.WithHttpsEndpoint(env: "HTTPS_PORT");
                 var httpsEndpoint = builder.GetEndpoint("https");
-
-                builder.WithEnvironment(context =>
+                
+                // Configure Node to trust the ASP.NET Core HTTPS development certificate as a root CA.
+                builder.WithEnvironment(async context =>
                 {
-                    // Configure Node to trust the ASP.NET Core HTTPS development certificate as a root CA.
-                    if (context.EnvironmentVariables.TryGetValue(certFileEnv, out var certPath))
-                    {
-                        context.EnvironmentVariables["NODE_EXTRA_CA_CERTS"] = certPath;
-                        context.EnvironmentVariables["HTTPS_REDIRECT_PORT"] = ReferenceExpression.Create($"{httpsEndpoint.Property(EndpointProperty.Port)}");
-                    }
+                    var logger = services.GetRequiredService<ResourceLoggerService>().GetLogger(builder.Resource);
+                    var (succeded, pemFilePath, _) = await DevCertHostingExtensions.TryExportDevCertificateAsync(CertificateFileFormat.Pem, builder.ApplicationBuilder, logger);
+                    context.EnvironmentVariables["NODE_EXTRA_CA_CERTS"] = pemFilePath;
+                    context.EnvironmentVariables["HTTPS_REDIRECT_PORT"] = ReferenceExpression.Create($"{httpsEndpoint.Property(EndpointProperty.Port)}");
                 });
+
+                return Task.CompletedTask;
             });
         }
 
---
+++ samples/AspireWithNode/AspireWithNode.AppHost/RedisHostingExtensions.cs
@@ -0,0 +1,53 @@
+using Microsoft.Extensions.Hosting;
+
+namespace Aspire.Hosting;
+
+internal static class RedisHostingExtensions
+{
+    /// <summary>
+    /// Configures the Redis resource to use the ASP.NET Core HTTPS developer certificate when
+    /// <paramref name="builder"/>.<see cref="IResourceBuilder{T}.ApplicationBuilder">ApplicationBuilder</see>.<see cref="IDistributedApplicationBuilder.ExecutionContext">ExecutionContext</see>.<see cref="DistributedApplicationExecutionContext.IsRunMode">IsRunMode</see><c> == true</c>.<br/>
+    /// </summary>
+    public static IResourceBuilder<RedisResource> RunWithHttpsDevCertificate(this IResourceBuilder<RedisResource> builder)
+    {
+        if (builder.ApplicationBuilder.ExecutionContext.IsRunMode && builder.ApplicationBuilder.Environment.IsDevelopment())
+        {
+            DevCertHostingExtensions.RunWithHttpsDevCertificate(builder, CertificateFileFormat.Pem, "TLS_CERT_FILE", "TLS_KEY_FILE", (services, certFilePath, certPasswordOrKeyPath) =>
+            {
+                // This callback is invoked during the BeforeStartEvent phase if the certificate is successfully exported.
+                builder.WithConnectionStringRedirection(new RedisTlsConnectionString(builder.Resource));
+
+                // Configure Redis to use the ASP.NET Core HTTPS development certificate.
+                builder.WithArgs(context =>
+                {
+                    context.Args.Add("--port");
+                    context.Args.Add("0");
+                    context.Args.Add("--tls-port");
+                    context.Args.Add(builder.Resource.PrimaryEndpoint.TargetPort.ToString()!);
+                    context.Args.Add("--tls-cert-file");
+                    context.Args.Add($"{DevCertHostingExtensions.DEV_CERT_BIND_MOUNT_DEST_DIR}/{DevCertHostingExtensions.DEV_CERT_FILE_NAME_PEM}");
+                    context.Args.Add("--tls-key-file");
+                    context.Args.Add($"{DevCertHostingExtensions.DEV_CERT_BIND_MOUNT_DEST_DIR}/{DevCertHostingExtensions.DEV_CERT_FILE_NAME_KEY}");
+                    context.Args.Add("--tls-ca-cert-file");
+                    context.Args.Add($"{DevCertHostingExtensions.DEV_CERT_BIND_MOUNT_DEST_DIR}/{DevCertHostingExtensions.DEV_CERT_FILE_NAME_PEM}");
+                    context.Args.Add("--tls-auth-clients");
+                    context.Args.Add("no");
+                });
+
+                return Task.CompletedTask;
+            });
+        }
+
+        return builder;
+    }
+
+    private class RedisTlsConnectionString(RedisResource resource) : IResourceWithConnectionString
+    {
+        public string Name { get; } = $"{resource.Name}-tls";
+
+        public ReferenceExpression ConnectionStringExpression => ReferenceExpression.Create(
+            $"{resource.PrimaryEndpoint.Property(EndpointProperty.Host)}:{resource.PrimaryEndpoint.Property(EndpointProperty.Port)},Ssl=true");
+
+        public ResourceAnnotationCollection Annotations { get; } = [];
+    }
+}
