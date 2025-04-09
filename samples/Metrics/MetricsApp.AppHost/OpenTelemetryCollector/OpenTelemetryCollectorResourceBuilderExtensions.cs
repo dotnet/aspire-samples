dc1e99a3db28c14825b3068306f69cc70859fa6c
Damian Edwards <damian@damianedwards.com>
2025-04-09 12:05:03 -0700

    Update env vars & use specific otel collector image tag

--- samples/Metrics/MetricsApp.AppHost/OpenTelemetryCollector/OpenTelemetryCollectorResourceBuilderExtensions.cs
+++ samples/Metrics/MetricsApp.AppHost/OpenTelemetryCollector/OpenTelemetryCollectorResourceBuilderExtensions.cs
@@ -4,9 +4,11 @@ namespace MetricsApp.AppHost.OpenTelemetryCollector;
 
 public static class OpenTelemetryCollectorResourceBuilderExtensions
 {
-    private const string DashboardOtlpUrlVariableName = "DOTNET_DASHBOARD_OTLP_ENDPOINT_URL";
+    private const string DashboardOtlpUrlVariableName = "ASPIRE_DASHBOARD_OTLP_ENDPOINT_URL";
     private const string DashboardOtlpApiKeyVariableName = "AppHost:OtlpApiKey";
     private const string DashboardOtlpUrlDefaultValue = "http://localhost:18889";
+    private const string OTelCollectorImageName = "ghcr.io/open-telemetry/opentelemetry-collector-releases/opentelemetry-collector-contrib";
+    private const string OTelCollectorImageTag = "0.123.0";
 
     public static IResourceBuilder<OpenTelemetryCollectorResource> AddOpenTelemetryCollector(this IDistributedApplicationBuilder builder, string name, string configFileLocation)
     {
@@ -19,7 +21,7 @@ public static class OpenTelemetryCollectorResourceBuilderExtensions
 
         var resource = new OpenTelemetryCollectorResource(name);
         var resourceBuilder = builder.AddResource(resource)
-            .WithImage("ghcr.io/open-telemetry/opentelemetry-collector-releases/opentelemetry-collector-contrib", "latest")
+            .WithImage(OTelCollectorImageName, OTelCollectorImageTag)
             .WithEndpoint(targetPort: 4317, name: OpenTelemetryCollectorResource.OtlpGrpcEndpointName, scheme: isHttpsEnabled ? "https" : "http")
             .WithEndpoint(targetPort: 4318, name: OpenTelemetryCollectorResource.OtlpHttpEndpointName, scheme: isHttpsEnabled ? "https" : "http")
             .WithBindMount(configFileLocation, "/etc/otelcol-contrib/config.yaml")
