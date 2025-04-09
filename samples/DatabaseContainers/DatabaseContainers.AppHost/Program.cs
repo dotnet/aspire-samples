794ae82d3e0ec0f62f44ad7ecbf75027db287a26
Damian Edwards <damian@damianedwards.com>
2025-04-09 14:31:32 -0700

    DatabaseContainers: MSSQL use new database creation script feature

--- samples/DatabaseContainers/DatabaseContainers.AppHost/Program.cs
+++ samples/DatabaseContainers/DatabaseContainers.AppHost/Program.cs
@@ -11,6 +11,7 @@ var postgres = builder.AddPostgres("postgres")
     .WithBindMount("../DatabaseContainers.ApiService/data/postgres", "/docker-entrypoint-initdb.d")
     // Configure the container to store data in a volume so that it persists across instances.
     .WithDataVolume()
+    .WithPgWeb()
     // Keep the container running between app host sessions.
     .WithLifetime(ContainerLifetime.Persistent);
 
@@ -37,19 +38,14 @@ var catalogDb = mysql.AddDatabase(catalogDbName);
 // SQL Server container is configured with an auto-generated password by default
 // but doesn't support any auto-creation of databases or running scripts on startup so we have to do it manually.
 var sqlserver = builder.AddSqlServer("sqlserver")
-    // Mount the init scripts directory into the container.
-    .WithBindMount("./sqlserverconfig", "/usr/config")
-    // Mount the SQL scripts directory into the container so that the init scripts run.
-    .WithBindMount("../DatabaseContainers.ApiService/data/sqlserver", "/docker-entrypoint-initdb.d")
-    // Run the custom entrypoint script on startup.
-    .WithEntrypoint("/usr/config/entrypoint.sh")
     // Configure the container to store data in a volume so that it persists across instances.
     .WithDataVolume()
     // Keep the container running between app host sessions.
     .WithLifetime(ContainerLifetime.Persistent);
 
 // Add the database to the application model so that it can be referenced by other resources.
-var addressBookDb = sqlserver.AddDatabase("AddressBook");
+var addressBookDb = sqlserver.AddDatabase("AddressBook")
+    .WithCreationScript(File.ReadAllText("../DatabaseContainers.ApiService/data/sqlserver/init.sql"));
 
 builder.AddProject<Projects.DatabaseContainers_ApiService>("apiservice")
     .WithReference(todosDb)
