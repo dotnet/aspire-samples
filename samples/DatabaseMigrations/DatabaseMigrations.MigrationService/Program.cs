6dc47daa0f44fc8274f829186e6a2dceefc5eba5
Damian Edwards <damian@damianedwards.com>
2025-04-09 15:39:06 -0700

    Remove SQL retry workaround in migrations sample

--- samples/DatabaseMigrations/DatabaseMigrations.MigrationService/Program.cs
+++ samples/DatabaseMigrations/DatabaseMigrations.MigrationService/Program.cs
@@ -5,20 +5,16 @@ using DatabaseMigrations.MigrationService;
 using Microsoft.EntityFrameworkCore;
 
 var builder = Host.CreateApplicationBuilder(args);
+
 builder.Services.AddHostedService<ApiDbInitializer>();
 
 builder.AddServiceDefaults();
 
 builder.Services.AddDbContextPool<MyDb1Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("db1"), sqlOptions =>
-    {
-        sqlOptions.MigrationsAssembly("DatabaseMigrations.MigrationService");
-        // Workaround for https://github.com/dotnet/aspire/issues/1023
-        sqlOptions.ExecutionStrategy(c => new RetryingSqlServerRetryingExecutionStrategy(c));
-    }));
-builder.EnrichSqlServerDbContext<MyDb1Context>(settings =>
-    // Disable Aspire default retries as we're using a custom execution strategy
-    settings.DisableRetry = true);
+        sqlOptions.MigrationsAssembly("DatabaseMigrations.MigrationService")
+    ));
+builder.EnrichSqlServerDbContext<MyDb1Context>();
 
 var app = builder.Build();
 
