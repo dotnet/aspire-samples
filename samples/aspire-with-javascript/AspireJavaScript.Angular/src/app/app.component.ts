9b57984f281df774a2d8747333586f9279dd83ce
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2025-12-16 19:26:45 -0500

    Fix to show weather data in the Angular app. Previously it was not rendering any data (#1382)
    * Fix to show weather data in the Angular app. Previously it was not rendering any data
    
    * Update based on Copilot feedback
    
    Changes in this commit
    
    - Removed aria labels as recommended by Copilot.
    - Change type `any` to `WeatherForecast` as recommended by Copilot. This required an update to the `Import` statement.
    - Changed weatherforecast url to be relative as recommended by Copilot.
    - Changed the constructor to use `ngOnInit` instead.
    
    I noticed that the data wasn't reliably showing up on the page. I had to add a change detector to get it to reliably appear.
    I'm not an Angular expert, so there may be a way to remove the change detector, but I wasn't able to figure it out.
    
    I think we can remove `@Injectable()` but I didn't want to add that to this PR.

--- samples/aspire-with-javascript/AspireJavaScript.Angular/src/app/app.component.ts
+++ samples/aspire-with-javascript/AspireJavaScript.Angular/src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component, Injectable } from '@angular/core';
+import { Component, Injectable, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { WeatherForecasts } from '../types/weatherForecast';
+import { WeatherForecast, WeatherForecasts } from '../types/weatherForecast';
 
 @Injectable()
 @Component({
@@ -12,14 +12,23 @@ import { WeatherForecasts } from '../types/weatherForecast';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'weather';
   forecasts: WeatherForecasts = [];
 
-  constructor(private http: HttpClient) {
-    http.get<WeatherForecasts>('api/weatherforecast').subscribe({
-      next: result => this.forecasts = result,
-      error: console.error
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) { }
+
+  ngOnInit(): void {
+    this.http.get<WeatherForecasts>('api/weatherforecast').subscribe({
+      next: result => {
+        this.forecasts = result;
+        this.cdr.detectChanges();
+      },
+      error: console.error,
     });
+  }
+
+  trackByDate(_index: number, forecast: WeatherForecast) {
+    return forecast.date;
   }
 }
